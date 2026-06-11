3aec2084fc2c7b800380dc706b8766ec64c07850
Lowis <157913738+Lowis-ar@users.noreply.github.com>
2026-06-10 23:05:18 -0600

    fix: correct Section import namespace for Filament v4 in BranchForm

--- app/Filament/Resources/Branches/Schemas/BranchForm.php
+++ app/Filament/Resources/Branches/Schemas/BranchForm.php
@@ -4,11 +4,11 @@ namespace App\Filament\Resources\Branches\Schemas;
 
 use Filament\Forms\Components\DatePicker;
 use Filament\Forms\Components\Repeater;
-use Filament\Forms\Components\Section;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\TimePicker;
 use Filament\Forms\Components\Toggle;
+use Filament\Schemas\Components\Section;
 use Filament\Schemas\Schema;
 
 class BranchForm
